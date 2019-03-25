242a6f83a438ad31a19b28098032a9faa6e0da73
Doyoung Gwak <tucan.dev@gmail.com>
2019-03-25 13:57:44 +0900

    first commit from original repo

---
+++ training/src/networks.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-3-6 3:20 PM
+# @Author  : zengzihua@huya.com
+# @FileName: data_filter.py
+# @Software: PyCharm
+
+import network_mv2_cpm
+import network_mv2_hourglass
+
+def get_network(type, input, trainable=True):
+    if type == 'mv2_cpm':
+        net, loss = network_mv2_cpm.build_network(input, trainable)
+    elif type == "mv2_hourglass":
+        net, loss = network_mv2_hourglass.build_network(input, trainable)        
+    return net, loss
