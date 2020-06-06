b36a65e2f902066adea91368933455c76a2570cf
Doyoung Gwak <tucan.dev@gmail.com>
2020-06-07 00:55:06 +0900

    no message

--- train_middlelayer.py
+++ train_middlelayer.py
@@ -293,8 +293,8 @@ if __name__ == '__main__':
     step = 1
     number_of_echo_period = 100
     number_of_validimage_period = 100000  # 1000
-    number_of_modelsave_period = 1000
-    tensorbaord_period = 10
+    number_of_modelsave_period = 5000
+    tensorbaord_period = 100
     validation_period = 10000  # 1000
     valid_check = False
 
